8ff0c666a5394048f6d98bfea3104f4babbe4eea
etashj <etashjhanji@gmail.com>
2025-04-17 10:30:44 -0400

    Initial commit. Plugins: telescope, treesitter, discord-rpc

---
+++ lua/config/options.lua
@@ -0,0 +1,12 @@
+-- enable line number and relative line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+
+--width of a time
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+
+--user number of spaces to inser a <Tab>
+vim.opt.expandtab = true
---
+++ lua/plugins/presence.lua
@@ -0,0 +1,11 @@
+return {
+  'andweeb/presence.nvim', 
+  opts = {
+    main_image = "file",
+    line_number_text    = "Line %s out of %s",
+    enable_line_number  = true,
+    buttons = {
+      {label = "GitHub", url = "https://github.com/etashj"},
+    },
+  }
+}
